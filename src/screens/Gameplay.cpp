49edeb473d6cf740e966b680ed55946720dc9c82
Nicodragon25 <nicolasariellaure@gmail.com>
2023-11-30 21:04:38 -0300

    Add GamePlay Timer

--- src/screens/Gameplay.cpp
+++ src/screens/Gameplay.cpp
@@ -2,6 +2,7 @@
 
 #include "entities/Target.h"
 #include "utils/Utilities.h"
+#include "constants/ScreenDimensions.h"
 #include "managers/FontsManager.h"
 
 namespace aimbotz
@@ -12,15 +13,20 @@ namespace aimbotz
 		static int totalHits = 0;
 		static float accuracy;
 
+		static int duration = 300;
+		static float timer;
+
+		Clock deltaClock;
 		void Init()
 		{
 			target::Init();
 			totalClicks = 0;
 			totalHits = 0;
+			timer = duration;
 		}
 
-		bool isMousePressed = false;
 
+		bool isMousePressed = false;
 		void Update(RenderWindow& window)
 		{
 			if (Mouse::isButtonPressed(Mouse::Left) && !isMousePressed)
@@ -39,6 +45,10 @@ namespace aimbotz
 
 			if (totalClicks > 0)
 				accuracy = (static_cast<float>(totalHits) / totalClicks) * 100;
+
+			Time dt = deltaClock.restart();
+			timer -= dt.asSeconds();
+
 		}
 
 		void Draw(RenderWindow& window)
@@ -49,10 +59,11 @@ namespace aimbotz
 			shape.setPosition(target::GetPosition());
 			shape.setFillColor(Color::Green);
 			window.draw(shape);
-
-			Text accuracyText;
+		
 			Font font;
 			font.loadFromFile("res/VT323.ttf");
+
+			Text accuracyText;
 			accuracyText.setFont(font);
 
 			accuracyText.setString(std::to_string(accuracy) + "%");
@@ -60,6 +71,16 @@ namespace aimbotz
 			accuracyText.setCharacterSize(50);
 			accuracyText.setFillColor(Color::Red);
 			window.draw(accuracyText);
+
+			Text timerText;
+			std::string timerMinutes = std::to_string(static_cast<int>(timer) / 60);
+			std::string timerSeconds = static_cast<int>(timer) % 60 >= 10 ? std::to_string(static_cast<int>(timer) % 60) : "0" +std::to_string(static_cast<int>(timer) % 60);
+			timerText.setFont(font);
+			timerText.setString(timerMinutes + ":" + timerSeconds);
+			timerText.setCharacterSize(50);
+			timerText.setFillColor(Color::Red);
+			timerText.setPosition(Vector2f(static_cast<float>(screen::SCREEN_WIDTH) / 2, 0));
+			window.draw(timerText);
 
 			window.display();
 		}
